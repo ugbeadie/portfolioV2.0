4c344a5774df983fe35ad924f766c67f8ecc2a76
ugbeadie <ugbeadie3@gmail.com>
2024-09-07 10:34:34 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-const navMenu = document.querySelector('nav');
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navUl = document.getElementsByClassName('nav-items')[0];
-const close = document.querySelector('close')
-const topLink = document.querySelector('.top-link')
+const navMenu = document.querySelector("nav");
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navUl = document.getElementsByClassName("nav-items")[0];
+const close = document.querySelector("close");
+const topLink = document.querySelector(".top-link");
 
-toggleButton.addEventListener('click', () => {
-    navUl.classList.toggle('active');
-    document.body.classList.toggle('hidden')
-    document.body.classList.toggle("cross");
-})
+toggleButton.addEventListener("click", () => {
+  navUl.classList.toggle("active");
+  document.body.classList.toggle("hidden");
+  document.body.classList.toggle("cross");
+});
 
-window.addEventListener('scroll',  function() {
-    if (window.scrollY > 550) {
-        navMenu.style.position = 'fixed';
-        navMenu.style.display = 'flex'
-        navMenu.style.backgroundColor = 'rgba(209, 210, 222, 0.8)';
-        topLink.classList.add("show-link");
-
-    }else{
-        navMenu.style.position = 'absolute';
-        navMenu.style.display = "none";
-        topLink.classList.remove("show-link");
-
-    }
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 550) {
+    navMenu.style.position = "fixed";
+    navMenu.style.display = "flex";
+    navMenu.style.backgroundColor = "rgba(209, 210, 222, 0.8)";
+    topLink.classList.add("show-link");
+  } else {
+    navMenu.style.position = "absolute";
+    navMenu.style.display = "none";
+    topLink.classList.remove("show-link");
+  }
 });
 
 function hideUl() {
-    document.body.classList.toggle("cross");
-    document.body.classList.toggle('hidden');
-    navUl.classList.toggle('active');  
+  document.body.classList.toggle("cross");
+  document.body.classList.toggle("hidden");
+  navUl.classList.toggle("active");
 }
 
 const date = document.getElementById("date");
